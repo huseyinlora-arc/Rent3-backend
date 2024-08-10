ccf23b35cfe0f75e0a42aca5a6743520a5514da6
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-08-10 21:53:25 +0300

    Added transaction hash validation with blockscout
    -- Validating transactionHashs with mode's sepolia blockscout API

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { DatabaseService } from "src/database/database.service";
 import { Prisma } from "@prisma/client";
 
@@ -6,8 +6,12 @@ import { Prisma } from "@prisma/client";
 export class PaymentsService {
   constructor(private readonly databaseService: DatabaseService) {}
   create(createPaymentDto: Prisma.RentalPaymentCreateInput) {
+    // Validate the payment transaction
+    if (!this.validateTransaction(createPaymentDto.transactionHash)) {
+      throw new HttpException("Forbidden", HttpStatus.BAD_REQUEST);
+    }
     // Create the payment
-    this.databaseService.rentalPayment.create({
+    const paymentData = this.databaseService.rentalPayment.create({
       data: createPaymentDto,
     });
 
@@ -18,10 +22,28 @@ export class PaymentsService {
       },
       where: { id: createPaymentDto.property.connect.id },
     });
+
+    return paymentData;
   }
 
   findAll() {
     return this.databaseService.rentalPayment.findMany();
+  }
+
+  async validateTransaction(transactionHash: string) {
+    const data: any = fetch(
+      `https://sepolia.explorer.mode.network/api/v2/transactions/${transactionHash}`,
+      {
+        headers: {
+          accept: "application/json",
+        },
+      },
+    )
+      .then((res) => res.json())
+      .catch((e) => {
+        console.log(e);
+      });
+    return data.status && data.status === "ok";
   }
 
   findOne(id: number) {
